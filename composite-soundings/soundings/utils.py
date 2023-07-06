d49e2a1f6b5ee1804d39ba688c6075f8df766a38
dkimpara <dkimpara@ucar.edu>
2023-07-06 14:59:03 -0600

    editing utils

--- composite-soundings/soundings/utils.py
+++ composite-soundings/soundings/utils.py
@@ -3,20 +3,44 @@ import xarray as xr
 import matplotlib.pyplot as plt
 
 from metpy.plots import SkewT
-from metpy.calc import (
-    relative_humidity_from_dewpoint,
-    mixing_ratio_from_relative_humidity,
-)
+from metpy.calc import relative_humidity_from_dewpoint,
 from metpy.units import units
 
-from wrf import wetbulb, enable_xarray
 import os
-
 import time
 
-# temp r, dpt bl, wetbulb  c (cyan)
-
 COLOR_DICT = {"t_h": "r", "dpt_h": "b", "wb_h": "c"}
+
+def skewCompositeFigAx(figsize=(5, 5), num_subplots=1):
+    #fig ax setup for skewx fig
+
+    if (num_subplots > 1) and (figsize == (5,5)):
+        figsize = (10,5)
+    fig, axs = plt.subplots(1, num_subplots, sharey=True,
+                            subplot_kw=dict(projection="skewx", rotation=30),
+                            figsize=figsize)
+    if num_subplots==1:
+        axs = [axs]
+    axs[0].set_ylabel("Height above ground (m)", fontsize=10)
+    for ax in axs:
+        ax.grid(which="both")
+
+        major_ticks = np.arange(-100, 100, 5)
+        ax.set_xticks(major_ticks)
+        ax.grid(which="major", alpha=0.5)
+
+        # minor_ticks = np.arange(xlowlim - 60, xhighlim, 1)
+        # ax.set_xticks(minor_ticks, minor=True)
+        # ax.grid(which='minor', alpha=0.2)
+
+        ax.axvline(x=0, ymin=0, ymax=1, c="0")
+        ax.set_ylim(-100, 5100)
+        ax.set_xlim(-15, 30)
+
+    return fig, axs
+
+########### data processing ###############
+
 BBOX = {
     "lon_min": 225.90453,
     "lon_max": 299.0828,
@@ -24,8 +48,19 @@ BBOX = {
     "lat_max": 52.615654,
 }
 
+def filter_latlon(ds):
+    mask = (
+        (ds.latitude <= BBOX["lat_max"])
+        & (ds.latitude >= BBOX["lat_min"])
+        & (ds.longitude <= BBOX["lon_max"])
+        & (ds.longitude >= BBOX["lon_min"])
+    )
+    return ds.where(mask.compute(), drop=True)
 
-def wb_stull(ds):
+def wb_stull(ds): 
+    # wrf.wetbulb is super slow because of calls to fortran
+    # this fn uses stulls method
+
     rh_h = relative_humidity_from_dewpoint(ds.t_h * units.degC, ds.dpt_h * units.degC)
     rh_h = rh_h.metpy.magnitude * 100
     T = ds.t_h
@@ -38,28 +73,61 @@ def wb_stull(ds):
     )
     return ds
 
+def frac_abv_zero(ds, x_col, total):
+    num_over_zero = (ds[x_col] > 0).any(dim="heightAboveGround").sum()
+    return num_over_zero / total
 
-def wet_bulb_from_rel_humid(ds):
-    enable_xarray()
-    rh_h = relative_humidity_from_dewpoint(ds.t_h * units.degC, ds.dpt_h * units.degC)
-    mr_h = mixing_ratio_from_relative_humidity(
-        ds.isobaricInhPa_h * units.hPa, ds.t_h * units.degC, rh_h
+
+def frac_abv_split_time(ds, x_cols):
+    np.seterr(divide="ignore", invalid="ignore")
+    ds = ds[x_cols]
+    num_over_zero = (ds > 0).any(dim="heightAboveGround").sum(dim=("x", "y"))
+    obs = (
+        ds[x_cols[0]]
+        .isel(heightAboveGround=0)
+        .count(dim=("x", "y"))
+        .drop_vars("heightAboveGround")
     )
-    ds["wb_h"] = wetbulb(
-        ds.isobaricInhPa_h * 100, ds.t_h + 273.15, mr_h, units="degC"
-    )  # output wbulb in degC
-    return ds
 
+    # if ((obs == 0).sum().values > 0) or (obs.isnull().sum().values > 0):
+    #    print(f'Warning: obs has a 0 val ({ds["time"]}, {ds.case_study_day})')
 
-def filter_latlon(ds):
-    mask = (
-        (ds.latitude <= BBOX["lat_max"])
-        & (ds.latitude >= BBOX["lat_min"])
-        & (ds.longitude <= BBOX["lon_max"])
-        & (ds.longitude >= BBOX["lon_min"])
+    return num_over_zero / obs
+
+########### data analysis ###############
+
+def count_nulls(ds):
+    nulls = ds.isnull().sum()
+
+    for var in list(nulls.keys()):
+        print(f"{var}: {nulls[var].values}")
+
+def count_notnulls(ds):
+    notnulls = ds.notnull().sum()
+
+    for var in list(notnulls.keys()):
+        print(f"{var}: {notnulls[var].values}")
+
+def timer(tic, message=""):
+    toc = time.time()
+    duration = toc - tic
+    minutes = int(duration / 60)
+    print(
+        f"{message}. Elapsed time: {str(minutes) + ' minutes, ' if minutes else ''}{int(duration % 60)} seconds"
     )
-    return ds.where(mask.compute(), drop=True)
 
+def fn_timer(fn):
+    tic = time.time()
+
+    out = fn()
+
+    toc = time.time()
+    duration = toc - tic
+    minutes = int(duration / 60)
+    print(
+        f"Elapsed time: {str(minutes) + ' minutes, ' if minutes else ''}{int(duration % 60)} seconds"
+    )
+    return out
 
 def open_ds_dkimpara(
     hour, model, cluster="casper", concat_dim="valid_time", parallel=False
@@ -92,94 +160,3 @@ def open_ds_dkimpara(
     # if "wb_h" not in list(ds.keys()):
     #    ds = wet_bulb_from_rel_humid(ds)
     return ds
-
-
-### refactoring to use linespecs in the future
-
-
-# set up the fig and ax
-def skewCompositeFigAx(figsize=(5, 5), num_subplots=1):
-    if (num_subplots > 1) and (figsize == (5,5)):
-        figsize = (10,5)
-    fig, axs = plt.subplots(1, num_subplots, sharey=True,
-                            subplot_kw=dict(projection="skewx", rotation=30),
-                            figsize=figsize)
-    if num_subplots==1:
-        axs = [axs]
-    axs[0].set_ylabel("Height above ground (m)", fontsize=10)
-    for ax in axs:
-        ax.grid(which="both")
-
-        major_ticks = np.arange(-100, 100, 5)
-        ax.set_xticks(major_ticks)
-        ax.grid(which="major", alpha=0.5)
-
-        # minor_ticks = np.arange(xlowlim - 60, xhighlim, 1)
-        # ax.set_xticks(minor_ticks, minor=True)
-        # ax.grid(which='minor', alpha=0.2)
-
-        ax.axvline(x=0, ymin=0, ymax=1, c="0")
-        ax.set_ylim(-100, 5100)
-        ax.set_xlim(-15, 30)
-
-    return fig, axs
-
-
-def frac_abv_zero(ds, x_col, total):
-    num_over_zero = (ds[x_col] > 0).any(dim="heightAboveGround").sum()
-    return num_over_zero / total
-
-
-def frac_abv_split_time(ds, x_cols):
-    np.seterr(divide="ignore", invalid="ignore")
-    ds = ds[x_cols]
-    num_over_zero = (ds > 0).any(dim="heightAboveGround").sum(dim=("x", "y"))
-    obs = (
-        ds[x_cols[0]]
-        .isel(heightAboveGround=0)
-        .count(dim=("x", "y"))
-        .drop_vars("heightAboveGround")
-    )
-
-    # if ((obs == 0).sum().values > 0) or (obs.isnull().sum().values > 0):
-    #    print(f'Warning: obs has a 0 val ({ds["time"]}, {ds.case_study_day})')
-
-    return num_over_zero / obs
-
-
-def count_nulls(ds):
-    nulls = ds.isnull().sum()
-
-    for var in list(nulls.keys()):
-        print(f"{var}: {nulls[var].values}")
-
-
-def count_notnulls(ds):
-    notnulls = ds.notnull().sum()
-
-    for var in list(notnulls.keys()):
-        print(f"{var}: {notnulls[var].values}")
-
-
-def timer(tic, message=""):
-    toc = time.time()
-    duration = toc - tic
-    minutes = int(duration / 60)
-    print(
-        f"{message}. Elapsed time: {str(minutes) + ' minutes, ' if minutes else ''}{int(duration % 60)} seconds"
-    )
-
-
-def fn_timer(fn):
-    tic = time.time()
-
-    out = fn()
-
-    toc = time.time()
-    duration = toc - tic
-    minutes = int(duration / 60)
-    print(
-        f"Elapsed time: {str(minutes) + ' minutes, ' if minutes else ''}{int(duration % 60)} seconds"
-    )
-
-    return out
