304d763d06e943d77e5c39b305436cac4cd72cf5
charlie-becker <cbecker@ucar.edu>
2023-11-17 18:21:55 -0700

    update config and submit (derecho queue)

--- scripts/submit_inference.sh
+++ scripts/submit_inference.sh
@@ -1,13 +1,13 @@
 #!/bin/bash -l
 #PBS -N ptype_inf
 #PBS -A NAML0001
-#PBS -l walltime=12:00:00
+#PBS -l walltime=00:30:00
 #PBS -o ptype_inference.out
 #PBS -e ptype_inference.out
-#PBS -q casper
-#PBS -l select=1:ncpus=24:mem=256GB
+#PBS -q main
+#PBS -l select=1:ncpus=20:ngpus=4:mem=128GB -l gpu_type=a100
 #PBS -m a
-#PBS -M dgagne@ucar.edu
+#PBS -M cbceker@ucar.edu
 conda activate ptype
-cd /glade/work/dgagne/ptype-physical/
-python -u ./scripts/run_inference.py  -c ./config/inference_vaisala_NE.yml
+cd /glade/work/cbecker/ptype-physical/
+python -u ./scripts/run_inference.py  -c ./config/inference.yml
